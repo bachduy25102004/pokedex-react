c2da1f8a154a74ed099b4aa4a37c89d6904c5d67
bachduy25102004 <bachduy25102004@gmail.com>
2025-08-12 21:18:42 +0700

    fix vite base url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://bachduy25102004.github.io/"
+  base: "https://bachduy25102004.github.io/pokedex-react/"
 })
